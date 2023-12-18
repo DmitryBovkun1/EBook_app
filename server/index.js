e88ffd44ac908c20470ffc385550ba2fa67fb9b1
DmitryBovkun1 <61903048+DmitryBovkun1@users.noreply.github.com>
2023-12-18 15:57:24 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,52 +1,52 @@
-﻿const express = require('express');
+﻿// Import necessary modules
+const express = require('express');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
-const app = express();
-const port = 3001;
-app.use(cors());
-app.use(express.json());
-let db;
-const connectToMongoDB = async () => {
-try {
-const client = await MongoClient.connect('mongodb://mongodb:27017/', { useUnifiedTopology: true });
-console.log('Connected to MongoDB');
-db = client.db('mybookdb');
-} catch (err) {
-console.error("MongoDB Connection Error: ", err);
-}
-}
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
+// Create an Express application
+const app = express();
+
+// Set the port for the server to listen on
+const port = 3001;
+
+// Enable Cross-Origin Resource Sharing (CORS)
+app.use(cors());
+
+// Enable JSON parsing for incoming requests
+app.use(express.json());
+
+// Initialize a variable to hold the MongoDB connection
+let db;
+
+// Function to connect to MongoDB
+const connectToMongoDB = async () => {
+    try {
+        // Connect to MongoDB server
+        const client = await MongoClient.connect('mongodb://127.0.0.1:27017/', { useUnifiedTopology: true });
+        console.log('Connected to MongoDB');
+        // Set the 'db' variable to the database
+        db = client.db('mybookdb');
+    } catch (err) {
+        console.error("MongoDB Connection Error: ", err);
     }
-});
+}
+
+// Endpoint to handle POST requests to add a book
 app.post('/books', (req, res) => {
-const book = req.body;
-db.collection('books').insertOne(book, (err, result) => {
-if (err) {
-console.error("Insert Error: ", err);
-return res.status(500).send(err);
-}
-res.status(201).send(result.ops[0]);
+    const book = req.body;
+    // Insert the book into the 'books' collection
+    db.collection('books').insertOne(book, (err, result) => {
+        if (err) {
+            console.error("Insert Error: ", err);
+            return res.status(500).send(err);
+        }
+        // Send the newly inserted book as a response
+        res.status(201).send(result.ops[0]);
+    });
 });
-});
-/*app.get('/books', (req, res) => {
-   db.collection('books').find({}).toArray((err, books) => {
-if (err) {
-console.error("Find Error: ", err);
-return res.status(500).send(err);
-}
-res.status(200).send(books);
-});
-}); 
-*/
+
+// Endpoint to handle GET requests to retrieve books based on author and title
 app.get('/books', (req, res) => {
     const { author, title } = req.query;
 
@@ -61,7 +61,7 @@ app.get('/books', (req, res) => {
         query.title = { $regex: new RegExp(title, 'i') };
     }
 
-    // Use the query object to filter books
+    // Use the query object to filter books and send the result as a response
     db.collection('books').find(query).toArray((err, books) => {
         if (err) {
             console.error("Find Error: ", err);
@@ -71,64 +71,41 @@ app.get('/books', (req, res) => {
     });
 });
 
+// Endpoint to handle PUT requests to update a book
 app.put('/books/:id', (req, res) => {
-const id = req.params.id;
-const updatedBook = req.body;
-db.collection('books').updateOne({ _id: new ObjectId(id) }, { $set: updatedBook }, (err, result) => {
-if (err) {
-console.error("Update Error: ", err);
-return res.status(500).send(err);
-}
-res.status(200).send(result);
-});
-});
-app.delete('/books/:id', (req, res) => {
-const id = req.params.id;
-db.collection('books').deleteOne({ _id: new ObjectId(id) }, (err, result) => {
-if (err) {
-console.error("Delete Error: ", err);
-return res.status(500).send(err);
-}
-res.status(200).send(result);
-});
-});
-connectToMongoDB().then(() => {
-app.listen(port, () => {
-console.log(`Server is running on http://localhost:${port}`);
-});
+    const id = req.params.id;
+    const updatedBook = req.body;
+    // Update the book with the specified ID in the 'books' collection
+    db.collection('books').updateOne({ _id: new ObjectId(id) }, { $set: updatedBook }, (err, result) => {
+        if (err) {
+            console.error("Update Error: ", err);
+            return res.status(500).send(err);
+        }
+        res.status(200).send(result);
+    });
 });
 
+// Endpoint to handle DELETE requests to delete a book
+app.delete('/books/:id', (req, res) => {
+    const id = req.params.id;
+    // Delete the book with the specified ID from the 'books' collection
+    db.collection('books').deleteOne({ _id: new ObjectId(id) }, (err, result) => {
+        if (err) {
+            console.error("Delete Error: ", err);
+            return res.status(500).send(err);
+        }
+        res.status(200).send(result);
+    });
+});
+
+// Connect to MongoDB and start the server
+connectToMongoDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+});
+
+// Endpoint to handle GET requests to the root URL
 app.get('/', (req, res) => {
     res.send('Welcome to my book API!');
 });
-
-/*
-const fs = require('fs'); // Підключення модуля 'fs' для роботи з файловою системою
-app.get('/', (req, res) => {
-fs.readFile('./my-ebooks-app/index.html', 'utf8', (err, data) => {
-   if (err) {
-       // Обробка помилок, якщо файл не може бути зчитаний
-       console.error(err);
-       res.status(500).send('Помилка зчитування файлу index.html');
-   } else {
-       // Відправка вмісту файлу як відповідь на запит
-       res.send(data);
-   }
-});
-
-});
-app.get('/my-ebooks-app/script.js', (req, res) => {
-    res.setHeader('Content-Type', 'text/javascript');
-    // Your code to send the script.js file
-    fs.readFile('./my-ebooks-app/script.js', 'utf8', (err, data) => {
-        if (err) {
-            // Обробка помилок, якщо файл не може бути зчитаний
-            console.error(err);
-            res.status(500).send('Помилка зчитування файлу index.html');
-        } else {
-            // Відправка вмісту файлу як відповідь на запит
-            res.send(data);
-        }
-     });
-  });
-  */
